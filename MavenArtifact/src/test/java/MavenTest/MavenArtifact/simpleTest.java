2208e41c3cb6054dea96405d90ef18b36147462f
JenkinsMagIch <57354405+JenkinsMagIch@users.noreply.github.com>
2019-11-04 13:12:32 +0100

    Add files via upload

---
+++ MavenArtifact/src/test/java/MavenTest/MavenArtifact/simpleTest.java
@@ -0,0 +1,36 @@
+package MavenTest.MavenArtifact;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class simpleTest
+		extends
+		TestCase {
+
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public simpleTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static TestSuite suite() {
+		return new TestSuite(simpleTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
+}
